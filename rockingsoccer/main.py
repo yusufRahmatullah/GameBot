52891b95c77d8d146be8e4fc5ffec36573e3ca7a
yusufRahmatullah <ycingle@gmail.com>
2016-12-06 12:48:44 +0700

    first commit

---
+++ rockingsoccer/main.py
@@ -0,0 +1,23 @@
+#! /user/bin/python
+
+from selenium import webdriver as wd
+import json
+
+class Main():
+	def __init__(self, username, password):
+		self.username = username
+		self.password = password
+	
+	def init(self):
+		self.b = wd.Chrome()
+		
+	def login():
+		self.b.get('http://rockingsoccer.com/en/soccer')
+
+if __name__ == '__main__':
+	with open('config.json', 'r') as f:
+		config = json.read(f)
+	username = config['username']
+	password = config['password']
+	m = Main(username, password);
+	m.login()
